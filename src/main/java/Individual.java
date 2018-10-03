a12c872e7c5522130bf85619f2252793e8a33bfa
Igor Pejic <igor.pejic@dr.com>
2018-10-03 19:20:18 +0200

    clean code; add python tuner

--- src/main/java/Individual.java
+++ src/main/java/Individual.java
@@ -2,6 +2,7 @@ import java.util.Random;
 import java.util.Arrays;
 import java.util.*;
 import java.lang.Math;
+import org.vu.contest.ContestEvaluation;
 
 
 public class Individual {
@@ -15,9 +16,12 @@ public class Individual {
     Double ranking_probability_linear;
     Double ranking_probability_exp;
 
+    ContestEvaluation evaluation;
+
     Double delta;
 
     List<Double> n_deltas;
+    List<Double> n_alphas;
 
     @Override
     public String toString() {
@@ -29,10 +33,11 @@ public class Individual {
         */
     }
 
-    public Individual(Random rnd, List<Double> genotype) {
+    public Individual(Random rnd, ContestEvaluation evaluation, List<Double> genotype) {
         this.genotype = genotype;
         this.rnd = rnd;
         this.age = 0;
+        this.evaluation = evaluation;
         if (this.genotype.size() != 10) {
             for (int i = 0; i < 10; i++) {
                 this.genotype.add(randomValueInDomain());
@@ -46,18 +51,16 @@ public class Individual {
         }
     }
 
-    public Individual(Random rnd) {
+    public Individual(Random rnd, ContestEvaluation evaluation) {
+        // best for bentcigar
         /*
-        this(rnd, Arrays.asList(
-                    // best for bentcigar
+        this(rnd, evaluation, Arrays.asList(
                     -1.1481615452524466, 4.002046649371003, -0.43178744966559485, -3.4897643863689924, 0.44376272902370223, -1.8086296469778993, 1.1882329735675545, -0.7882132938335314, 1.5181745335044008, -0.42285650091772453
                     //-1.1481615452524465, 4.491617537557367, -0.43178744966559485, -3.12080777515226, 0.44376272902370223, -1.8086296469778993, 1.1882329735675545, -0.7882132938335314, 1.368928057796371, -0.42285650091772453
                     ));
-        */
-        this(rnd, new ArrayList<Double>(10));
-        /*
-        this(rnd, Arrays.asList(
-                    ));
+                    */
+        this(rnd, evaluation, new ArrayList<Double>(10));
+
         /*
         this(rnd, Arrays.asList(
                     3.664747911894554, 2.5402683593118547, -1.530645323979111, 1.4463744845556485, 1.377491652390991, -1.8890949554704584, 3.4909693360640537, -2.3250308866867107, -0.3753226786821977, -2.0321133319786613
@@ -122,24 +125,57 @@ public class Individual {
     }
 
     public void UncorrelatedMutationOneStepSize() {
+        double epsilon = 0.00000001;
         double tau = 1 / Math.sqrt(POPULATION_SIZE);
 
         int randomAllele = rnd.nextInt((9 - 0) + 1) + 0;
         this.delta = this.delta * Math.exp(tau * rnd.nextGaussian());
+        if (this.delta < epsilon) {
+            this.delta = epsilon; 
+        }
         //System.out.println(this.delta);
         this.genotype.set(randomAllele, this.genotype.get(randomAllele) + this.delta * rnd.nextGaussian());
     }
 
-    public void UncorrelatedMutationNStepSizes() {
-        double tau = 1  / (Math.sqrt(2 * Math.sqrt(POPULATION_SIZE)));
-        double tau_prime = 1 / (Math.sqrt(2 * POPULATION_SIZE));
+    public void UncorrelatedMutationNStepSizes(double epsilon, double first_arg, double second_arg) {
+        double tau = 1  / (Math.sqrt(first_arg * Math.sqrt(POPULATION_SIZE)));
+        double tau_prime = 1 / (Math.sqrt(second_arg * POPULATION_SIZE));
 
         int randomAllele = rnd.nextInt((9 - 0) + 1) + 0;
 
-        this.n_deltas.set(randomAllele,
-                this.n_deltas.get(randomAllele) * Math.exp(tau_prime * rnd.nextGaussian() + 
-                    tau * rnd.nextGaussian()));
+
+        double new_delta = this.n_deltas.get(randomAllele) * Math.exp(tau_prime * rnd.nextGaussian() + 
+                    tau * rnd.nextGaussian());
+
+        if (new_delta < epsilon) {
+            new_delta = epsilon;
+        }
+        this.n_deltas.set(randomAllele,new_delta);
 
         this.genotype.set(randomAllele, this.genotype.get(randomAllele) + this.n_deltas.get(randomAllele) * rnd.nextGaussian());
     }
+
+    /*
+    public void CorrelatedMutation(double epsilon, double first_arg, double second_arg) {
+        double tau = 1  / (Math.sqrt(first_arg * Math.sqrt(POPULATION_SIZE)));
+        double tau_prime = 1 / (Math.sqrt(second_arg * POPULATION_SIZE));
+        double beta = 5;
+
+        int randomAllele = rnd.nextInt((9 - 0) + 1) + 0;
+
+        double new_delta = this.n_deltas.get(randomAllele) * Math.exp(tau_prime * rnd.nextGaussian() + 
+                    tau * rnd.nextGaussian());
+
+        double new_alpha = this.n_alphas.get(randomAllele) + beta * rnd.nextGaussian();
+
+        if (new_delta < epsilon) {
+            new_delta = epsilon;
+        }
+        this.n_deltas.set(randomAllele, new_delta);
+        this.n_alphas.set(randomAllele, new_alpha);
+
+        this.genotype.set(randomAllele, this.genotype.get(randomAllele) + this.n_deltas.get(randomAllele) * rnd.nextGaussian());
+    }
+
+    */
 }
---
+++ src/main/java/tuner.py
@@ -0,0 +1,132 @@
+import os
+import csv
+import numpy as np
+import subprocess
+from collections import OrderedDict
+
+import sys
+import fileinput
+import shutil
+import argparse
+
+
+def main(schaffers=False, katsuura=False):
+
+    if not schaffers and not katsuura:
+        # bentcigar parameters
+        parameters_dict = OrderedDict([
+            ('ISLANDS_NUMBER', np.linspace(1, 1000, 1, dtype='int')),
+            ('POPULATION_SIZE', np.linspace(300, 1000, 1, dtype='int')),
+            ('CHILDREN_SIZE', np.linspace(10, 100, 1, dtype='int')),
+            ('PARENTS_SIZE', np.linspace(0.7, 0.8, 1, )),
+            ('ARITHMETIC_XOVER_N_PARENTS', np.linspace(2, 8, 1, dtype='int')),
+            ('MUTATION_PROBABILITY', np.linspace(0.55333, 0.56, 1)),
+            ('N_SURVIVORS', np.linspace(100, 100, 1, dtype='int')),
+            ('TOURNAMENT_SIZE', np.linspace(3, 10, 1, dtype='int')),
+            ('ARITHMETIC_RECOMB_ALPHA', np.linspace(0.11, 0.42, 1)),
+            ('MUTATION_A', np.linspace(2.388888, 2.5, 1)),
+            ('MUTATION_B', np.linspace(2.1666, 2.5, 1)),
+            ('MUTATION_EPSILON', np.linspace(5.52773266332e-06, 5.52773266332e-06, 1)),
+            ('MIGRATION_AFTER_EPOCHS', np.linspace(150, 150, 1, dtype='int')),
+            ('ELITISM_TO_KEEP', np.linspace(0, 0, 1, dtype='int'))
+        ])
+    elif katsuura:
+        parameters_dict = OrderedDict([
+            ('ISLANDS_NUMBER', np.linspace(10, 20, 1, dtype='int')),
+            ('POPULATION_SIZE', np.linspace(100, 200, 5, dtype='int')),
+            ('CHILDREN_SIZE', np.linspace(10, 100, 1, dtype='int')),
+            ('PARENTS_SIZE', np.linspace(0.1, 0.8, 1, )),
+            ('ARITHMETIC_XOVER_N_PARENTS', np.linspace(2, 8, 1, dtype='int')),
+            ('MUTATION_PROBABILITY', np.linspace(0.33, 0.56, 1)),
+            ('N_SURVIVORS', np.linspace(100, 100, 1, dtype='int')),
+            ('TOURNAMENT_SIZE', np.linspace(3, 10, 1, dtype='int')),
+            ('ARITHMETIC_RECOMB_ALPHA', np.linspace(0.11, 0.42, 1)),
+            ('MUTATION_A', np.linspace(2.388888, 2.5, 1)),
+            ('MUTATION_B', np.linspace(2.1666, 2.5, 1)),
+            ('MUTATION_EPSILON', np.linspace(5.52773266332e-06, 5.52773266332e-06, 1)),
+            ('MIGRATION_AFTER_EPOCHS', np.linspace(10, 150, 10, dtype='int')),
+            ('ELITISM_TO_KEEP', np.linspace(10, 20, 6, dtype='int'))
+        ])
+
+    with open('results.csv', 'w') as outfile:
+        fieldnames = list(parameters_dict.keys())
+        fieldnames.insert(0, 'SCORE')
+        spamwriter = csv.writer(outfile, delimiter=';')
+        spamwriter.writerow(fieldnames)
+
+    for ELITISM_TO_KEEP in parameters_dict['ELITISM_TO_KEEP']:
+        for MIGRATION_AFTER_EPOCHS in parameters_dict['MIGRATION_AFTER_EPOCHS']:
+            for ISLANDS_NUMBER in parameters_dict['ISLANDS_NUMBER']:
+                for MUTATION_EPSILON in parameters_dict['MUTATION_EPSILON']:
+                    for MUTATION_A in parameters_dict['MUTATION_A']:
+                        for MUTATION_B in parameters_dict['MUTATION_B']:
+                            for ARITHMETIC_RECOMB_ALPHA in parameters_dict['ARITHMETIC_RECOMB_ALPHA']:
+                                for TOURNAMENT_SIZE in parameters_dict['TOURNAMENT_SIZE']:
+                                    for PARENTS_SIZE in parameters_dict['PARENTS_SIZE']:
+                                        for CHILDREN_SIZE in parameters_dict['CHILDREN_SIZE']:
+                                            for POPULATION_SIZE in parameters_dict['POPULATION_SIZE']:
+                                                for ARITHMETIC_XOVER_N_PARENTS in parameters_dict['ARITHMETIC_XOVER_N_PARENTS']:
+                                                    for MUTATION_PROBABILITY in parameters_dict['MUTATION_PROBABILITY']:
+                                                        for N_SURVIVORS in parameters_dict['N_SURVIVORS']:
+                                                            parameters = OrderedDict()
+                                                        for key in parameters_dict.keys():
+                                                            parameters[key] = locals()[key]
+                                                            if N_SURVIVORS > POPULATION_SIZE:
+                                                                continue
+                                                        edit_java_file(parameters)
+                                                        compile_and_run_5_times(
+                                                            parameters, schaffers=schaffers, katsuura=katsuura)
+
+
+def edit_java_file(parameters):
+    shutil.copy('player18.template.java', 'player18.java')
+    for k, v in parameters.items():
+        for i, line in enumerate(fileinput.input('player18.java', inplace=1)):
+            sys.stdout.write(line.replace(k + '_TUNERXXX', str(v)))
+
+
+def compile_and_run_5_times(parameters, schaffers=False, katsuura=False):
+    scores = []
+    exceptioned = False
+    halted_bad_result = False
+    function = 'BentCigarFunction'
+    if schaffers:
+        function = 'SchaffersEvaluation'
+    elif katsuura:
+        function = 'KatsuuraEvaluation'
+    for i in range(3):
+        ret = subprocess.check_output('javac -cp contest.jar player18.java Individual.java Island.java && jar cmf MainClass.txt submission.jar *class && java -jar testrun.jar -submission=player18 -evaluation={} -seed={}'.format(function, i), shell=True)
+
+        score = str(ret).split('Score: ')[1].split('\\n')[0]
+        score = float(score)
+        scores.append(score)
+        if 'halt' in str(ret) and score < 1.0:
+            exceptioned = True
+            break
+
+        if score < 4.0:
+            halted_bad_result = True
+            break
+
+    average_score = sum(scores) / len(scores)
+    print(average_score, len(scores))
+
+    with open('results.csv', 'a') as outfile:
+        spamwriter = csv.writer(outfile, delimiter=';')
+        values_list = list(parameters.items())
+        values_list = [v[1] for v in values_list]
+        values_list.insert(0, average_score)
+        if exceptioned:
+            values_list.append('EXCEPTIONED')
+        if halted_bad_result:
+            values_list.append('HALTED_BAD_RESULT')
+        spamwriter.writerow(values_list)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Tuner run')
+    parser.add_argument('--schaffers', action="store_true", default=False)
+    parser.add_argument('--katsuura', action="store_true", default=False)
+
+    args = parser.parse_args()
+    main(args.schaffers, args.katsuura)
